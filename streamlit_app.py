47e82669ead54f3719a01c3aba19638a1df33617
shaumimy <embaranishaumy@gmail.com>
2025-04-24 13:20:17 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,5 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("🎈 hallo i'm shaumy)
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+    "nungguin ya, haloooo bess 
